b994c2a3436173ef864d98e8ace8108983fa2d8d
Jingxiang-Zhang <1967527237@qq.com>
2023-06-10 16:00:07 -0700

    Context and OpenGL Context

--- GameEngine/src/Platform/OpenGL/OpenGLContext.cpp
+++ GameEngine/src/Platform/OpenGL/OpenGLContext.cpp
@@ -20,9 +20,9 @@ namespace GE {
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		GE_CORE_ASSERT(status, "Failed to initialize Glad!");
 		GE_CORE_INFO("OpenGL Info:");
-		// GE_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
-		// GE_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
-		// GE_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
+		GE_CORE_INFO("  Vendor: {0}", (const char*)glGetString(GL_VENDOR));
+		GE_CORE_INFO("  Renderer: {0}", (const char*)glGetString(GL_RENDERER));
+		GE_CORE_INFO("  Version: {0}", (const char*)glGetString(GL_VERSION));
 
 		GE_CORE_ASSERT(GLVersion.major > 4 || (GLVersion.major == 4 && GLVersion.minor >= 5), "GameEngine requires at least OpenGL version 4.5!");
 	}
