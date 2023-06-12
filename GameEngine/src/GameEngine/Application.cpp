2a2e720627a9acc7b9d119821d369aecf47ba133
Jingxiang-Zhang <1967527237@qq.com>
2023-06-11 22:33:02 -0700

    Vertex Buffer layouts

--- GameEngine/src/GameEngine/Application.cpp
+++ GameEngine/src/GameEngine/Application.cpp
@@ -13,6 +13,28 @@ namespace GE
 
 	Application* Application::s_Instance = nullptr;
 
+	// temporary
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+		case ShaderDataType::Float:    return GL_FLOAT;
+		case ShaderDataType::Float2:   return GL_FLOAT;
+		case ShaderDataType::Float3:   return GL_FLOAT;
+		case ShaderDataType::Float4:   return GL_FLOAT;
+		case ShaderDataType::Mat3:     return GL_FLOAT;
+		case ShaderDataType::Mat4:     return GL_FLOAT;
+		case ShaderDataType::Int:      return GL_INT;
+		case ShaderDataType::Int2:     return GL_INT;
+		case ShaderDataType::Int3:     return GL_INT;
+		case ShaderDataType::Int4:     return GL_INT;
+		case ShaderDataType::Bool:     return GL_BOOL;
+		}
+
+		GE_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
+
 	Application::Application()
 	{
 		GE_CORE_ASSERT(s_Instance == nullptr, "Application already exist!");
@@ -29,16 +51,33 @@ namespace GE
 		glBindVertexArray(m_VertexArray);
 		
 		// generate vertex buffer and buffer data
-		float vertices[3 * 3] = { // default clip space: [-1 1]
-			-0.5f, -0.5f, 0.0f,
-			0.5f, -0.5f, 0.0f,
-			0.5f, 0.5f, 0.0f
+		float vertices[3 * 7] = { // default clip space: [-1 1]
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			0.5f, 0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f,
 		};
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-		// upload to the default shader
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		{
+			BufferLayout layout = {
+				{ShaderDataType::Float3, "a_Position" },
+				{ShaderDataType::Float4, "a_Color" }
+			};
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		uint32_t index = 0;
+		auto layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index, element.GetComponentCount(),
+				ShaderDataTypeToOpenGLBaseType(element.Type), 
+				element.Normalized ? GL_TRUE : GL_FALSE, layout.GetStride(), (const void*)element.Offset);
+			index++;
+		}
+
+		
 		
 		// generate index buffer
 		uint32_t indices[3] = { 0, 1, 2 };
@@ -48,9 +87,10 @@ namespace GE
 		std::string vertexSrc = R"(
 			#version 330 core
 			layout(location = 0) in vec3 a_Position;
-			out vec3 v_Position;
+			layout(location = 1) in vec4 a_Color;
+			out vec4 v_Color;
 			void main() {
-				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0);
 			}
 		)";
@@ -58,9 +98,9 @@ namespace GE
 		std::string fragmentSrc = R"(
 			#version 330 core
 			layout(location = 0) out vec4 color;
-			in vec3 v_Position;
+			in vec4 v_Color;
 			void main() {
-				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}
 		)";
 
